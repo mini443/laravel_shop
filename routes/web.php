8c9c7729e920b609f9b25df718ef5113f14e183e
minininja <834487783@qq.com>
2019-06-14 04:11:29 +0000

    邮箱验证

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'PagesController@root')->name('root');
-Auth::routes();
+Auth::routes(['verify' => true]);
 
