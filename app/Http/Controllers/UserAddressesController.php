6baaac30d3f7e83a30e3d46af45a209d3d9fd860
minininja <834487783@qq.com>
2019-06-17 06:15:01 +0000

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -25,6 +25,10 @@ class UserAddressesController extends Controller
         return view('user_addresses.create_and_edit',['address'=>new UserAddress()]);
     }
 
+    /**
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(UserAddressRequest $request){
         $request->user()->addresses()->create($request->only([
             'province',
@@ -37,5 +41,40 @@ class UserAddressesController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(UserAddress $user_address){
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    /**
+     * @param UserAddress $userAddress
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserAddress $userAddress,UserAddressRequest $request){
+        $userAddress->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+
+    public function destroy(UserAddress $user_address){
+        $this->authorize('own',$user_address);
+        $user_address->delete();
+//        return redirect()->route('user_addresses.index');
+        return [];
     }
 }
