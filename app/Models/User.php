8224faf2ef14771b0ecbf7fc5c4b1668368e1176
minininja <834487783@qq.com>
2019-06-18 11:05:14 +0000

    用户界面-商品收藏

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,15 @@ class User extends Authenticatable implements MustVerifyEmail
     public function addresses(){
         return $this->hasMany(UserAddress::class);
     }
+
+    /**
+     * 用户
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function favoriteProducts()
+    {
+        return $this->belongsToMany(Product::class, 'user_favorite_products')
+            ->withTimestamps()
+            ->orderBy('user_favorite_products.created_at', 'desc');
+    }
 }
